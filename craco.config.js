58aff1343241bc7f12731707b719a3372db94f87
Demons <oncwnuA-Qman3Lxb6_AFTz4gOqEU@git.weixin.qq.com>
2023-04-15 00:50:29 +0800

    fix: craco config

--- craco.config.js
+++ craco.config.js
@@ -155,6 +155,18 @@ module.exports = {
       },
     },
   ],
+  resolve: {
+    fallback: {
+      // crypto: require.resolve('crypto-browserify'),
+      // stream: require.resolve('stream-browserify'),
+      // assert: require.resolve('assert'),
+      // http: require.resolve('stream-http'),
+      // https: require.resolve('https-browserify'),
+      // os: require.resolve('os-browserify'),
+      // url: require.resolve('url'),
+      buffer: require.resolve('buffer/'),
+    },
+  },
   // 开发服务器
   devServer: {
     host: 'localhost', // 启动服务器域名
